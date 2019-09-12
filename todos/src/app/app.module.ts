57bc55598ee9e77504b50bcd925cf6863af6016e
t0m <thomas.goodheart@wagner.edu>
2019-09-12 03:02:12 -0400

    added in routing package, imports app.mod.ts

--- todos/src/app/app.module.ts
+++ todos/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+
+const routes: Routes = [
+  { path: ':status', component: TodoComponent },
+  { path: '**', redirectTo: '/all' }
+];
 
 @NgModule({
   declarations: [
@@ -15,6 +23,7 @@ import { TodoComponent } from './todo/todo.component';
     BrowserModule,
     AppRoutingModule,
 		FormsModule,
+		RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
